4935c215450f33a58a300f27cff193070e0322b7
Joshua <joshua.sinclairchong@mymail.champlain.edu>
2023-09-12 12:43:06 -0400

    Added shader.cpp/h

---
+++ core/jsc/shader.cpp
@@ -0,0 +1,15 @@
+#include "shader.h"
+
+namespace jsc {
+	std::string loadShaderSourceFromFile(const std::string& filePath) {
+		std::ifstream fstream(filePath);
+		if (!fstream.is_open()) {
+			printf("Failed to load file %s", filePath);
+			return {};
+		}
+
+		std::stringstream buffer;
+		buffer << fstream.rdbuf();
+		return buffer.str();
+	}
+}
